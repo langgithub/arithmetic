21b8a8d5d04d05a8d1eb0cd7d04113c34ec68585
langgithub <2031923407@qq.com>
2017-09-11 15:28:58 +0800

    快排

---
+++ src/com/lang/algorithm/sort/QuickSort.java
@@ -0,0 +1,41 @@
+package com.lang.algorithm.sort;
+
+/**
+ * Created by root on 2017/9/11.
+ */
+public class QuickSort {
+
+    public void sort(int[] arr,int left,int right){
+
+        if (arr==null||left>=right)
+            return;
+        int temp=arr[left];
+        int i=left+1,j=right;
+        while (i<j){
+            while (arr[j]>=temp&&i<j){
+                j--;
+            }
+            while (arr[i]<temp&&i<j){
+                i++;
+            }
+            if(i<j){
+                int m=arr[i];
+                arr[i]=arr[j];
+                arr[j]=m;
+            }
+        }
+        arr[left]=arr[i];
+        arr[i]=temp;
+        sort(arr,left,i-1);
+        sort(arr,i+1,right);
+
+    }
+    public static void main(String[] args){
+        int[] arr={6,3,7,8,6};
+        QuickSort quickSort=new QuickSort();
+        quickSort.sort(arr,0,arr.length-1);
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
